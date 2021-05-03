14297e5c6930a26d3444c27e829eb1f0471fc872
turinglabsorg <s.cataudo@turinglabs.org>
2021-05-03 09:04:27 +0200

    other fix

--- src/main.ts
+++ src/main.ts
@@ -305,7 +305,7 @@ app.get('/contracts', async (req, res) => {
   const contracts = await Track.find().sort({ name: 1 })
   for (let k in contracts) {
     let sc = contracts[k].smart_contract.trim().toUpperCase()
-    if (unique.indexOf(sc) === -1 && sc.name !== "") {
+    if (unique.indexOf(sc) === -1 && sc.name !== undefined && sc.name.length > 0) {
       unique.push(sc)
       response.push(contracts[k])
     }
